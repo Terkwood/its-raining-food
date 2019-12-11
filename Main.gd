d485adb0b87810c885e1b6467a7b9dd17a1bcbae
Terkwood <metaterkhorn@gmail.com>
2019-12-11 12:04:00 -0500

    Start to exist
    Squashed commit of the following:
    
    commit 13688cd113cd46065546ce17782653e1042205e2
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Wed Dec 11 12:03:18 2019 -0500
    
        Add impl and assets
    
    commit 4fcf32fb4e822e907faaa4f97bff46b81ffa9ab2
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Wed Dec 11 11:22:24 2019 -0500
    
        Update script

--- Main.gd
+++ Main.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
-var GodotBot = preload("res://GodotBot.tscn")
+var Bucket = preload("res://Bucket.tscn")
+
+var current = {}
 
 func _input(event):
 	if event is InputEventScreenTouch and event.is_pressed():
-		add_child(GodotBot.instance())
+		var i = Bucket.instance()
+		i.position = event.position
+		current[event.index] = i
+		add_child(i)
+	if event is InputEventScreenTouch and not event.is_pressed() and current.has(event.index):
+		current[event.index].queue_free()
+		current.erase(event.index)
+	if event is InputEventScreenDrag and current.has(event.index):
+		current[event.index].position = current[event.index].position + event.relative
---
+++ README.md
@@ -0,0 +1,9 @@
+# It's Raining Food
+
+A game where you catch falling food.
+
+## Attributions
+
+Thank you to the folling open source contributors
+
+- [Lanea Zimmerman (AKA Sharm)](https://opengameart.org/users/sharm) for the bucket image submitted to [Liberated Pixel Cup](https://opengameart.org/content/liberated-pixel-cup-lpc-base-assets-sprites-map-tiles)
