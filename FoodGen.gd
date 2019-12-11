43617506d20fc1f047352a5b76d48781a8fac22b
Terkwood <metaterkhorn@gmail.com>
2019-12-11 17:52:29 -0500

    Update FoodGen.gd

--- FoodGen.gd
+++ FoodGen.gd
@@ -23,14 +23,16 @@ const all_food = [
 var time = 0
 var next = 0
 
+var half_window_size_x = OS.get_window_size().x / 2.0
+
 func _ready():
-	position.x = OS.get_window_size().x / 2.0
+	position.x = half_window_size_x
 
 func _physics_process(delta):
 	time = time + delta
 	if time >= next:
 		var i = all_food[randi() % all_food.size()].instance()
-		var pos_offset_x = randi() % 500
+		var pos_offset_x = randi() % int(half_window_size_x)
 		if randi() % 2 < 1:
 			pos_offset_x = pos_offset_x * -1
 		i.position.x = pos_offset_x
